cef4ea48e229d75c9dc449bf151822ee6495c2b5
Alice <alice.millour@abtela.eu>
2019-06-25 15:10:59 +0200

    add poetess for demo

--- Game_of_the_lost_words/action.php
+++ Game_of_the_lost_words/action.php
@@ -8,10 +8,22 @@ switch($_GET['action'])
      //number2 = (int)$_GET['number2'];
      //result = number1 + number2;
      
-     echo "\nPHP : In store_text \n";
+     echo "\nPHP : In store_text (php)\n";
      echo $_GET['text'];
+     echo "sending file_put_contents";
      echo file_put_contents("flower-text.txt",$_GET['text'],FILE_APPEND); 
      file_put_contents("flower-text.txt","\n",FILE_APPEND);
+     break;    
+  case 'store_rhyme': 
+     //number1 = (int)$_GET['number1'];
+     //number2 = (int)$_GET['number2'];
+     //result = number1 + number2;
+     
+     echo "\nPHP : In store_text (php)\n";
+     echo $_GET['text'];
+     echo "sending file_put_contents";
+     echo file_put_contents("nuage-text.txt",$_GET['text'],FILE_APPEND); 
+     file_put_contents("nuage-text.txt","\n",FILE_APPEND);
      break;  
 
 } 
