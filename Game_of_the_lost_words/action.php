97b498a8d3b558c11331015ccf47594662bc914d
Alice <alice.millour@abtela.eu>
2020-02-04 11:02:10 +0100

    add seamstress

--- Game_of_the_lost_words/action.php
+++ Game_of_the_lost_words/action.php
@@ -24,6 +24,17 @@ switch($_GET['action'])
      echo "sending file_put_contents";
      echo file_put_contents("nuage-text.txt",$_GET['text'],FILE_APPEND); 
      file_put_contents("nuage-text.txt","\n",FILE_APPEND);
+     break;   
+   case 'store_bibs': 
+     //number1 = (int)$_GET['number1'];
+     //number2 = (int)$_GET['number2'];
+     //result = number1 + number2;
+     
+     echo "\nPHP : In store_text (php)\n";
+     echo $_GET['text'];
+     echo "sending file_put_contents";
+     echo file_put_contents("bibs-text.txt",$_GET['text'],FILE_APPEND); 
+     file_put_contents("bibs-text.txt","\n",FILE_APPEND);
      break;  
   case 'store_song': 
      //number1 = (int)$_GET['number1'];
